1203cf52bf1828660dcc2c8f9ceed8a5b54ae0ff
Andrew Knowles <ajknowles@optonline.net>
2020-07-11 16:24:55 -0400

    Remove unused files
    Moving piston block did not need a mixin

---
+++ MixinLanternBlock.java
@@ -0,0 +1,94 @@
+package lettas.chains_link.mixin;
+
+import net.minecraft.block.*;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.piston.PistonBehavior;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.Properties;
+import net.minecraft.state.property.Property;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.WorldAccess;
+import net.minecraft.world.WorldView;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+
+@Mixin(LanternBlock.class)
+public class MixinLanternBlock extends Block implements Waterloggable {
+    private static final BooleanProperty HANGING;
+    private static final BooleanProperty WATERLOGGED;
+
+    public MixinLanternBlock(Settings settings) {
+        super(settings);
+        this.setDefaultState((BlockState)((BlockState)this.stateManager.getDefaultState()).with(HANGING, false).with(WATERLOGGED, false));
+    }
+
+    @Overwrite
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        Direction[] var2 = ctx.getPlacementDirections();
+        int var3 = var2.length;
+        FluidState fluidState = ctx.getWorld().getFluidState(ctx.getBlockPos());
+        boolean bl = fluidState.getFluid() == Fluids.WATER;
+
+        for(int var4 = 0; var4 < var3; ++var4) {
+            Direction direction = var2[var4];
+            if (direction.getAxis() == Direction.Axis.Y) {
+                BlockState blockState = (BlockState)this.getDefaultState().with(HANGING, direction == Direction.UP).with(WATERLOGGED, bl);
+                if (blockState.canPlaceAt(ctx.getWorld(), ctx.getBlockPos())) {
+                    return blockState;
+                }
+            }
+        }
+
+
+
+        return null;
+    }
+
+
+    @Overwrite
+    public PistonBehavior getPistonBehavior(BlockState state) {
+        return PistonBehavior.NORMAL;
+    }
+
+    @Overwrite
+    public static Direction attachedDirection(BlockState state) {
+        return (Boolean)state.get(HANGING) ? Direction.DOWN : Direction.UP;
+    }
+
+    @Overwrite
+    public boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
+        Direction direction = attachedDirection(state).getOpposite();
+        return Block.sideCoversSmallSquare(world, pos.offset(direction), direction.getOpposite());
+    }
+
+    @Overwrite
+    public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState newState, WorldAccess world, BlockPos pos, BlockPos posFrom) {
+        if ((Boolean)state.get(WATERLOGGED)) {
+            world.getFluidTickScheduler().schedule(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
+        }
+        return attachedDirection(state).getOpposite() == direction && !state.canPlaceAt(world, pos) ? Blocks.AIR.getDefaultState() : super.getStateForNeighborUpdate(state, direction, newState, world, pos, posFrom);
+    }
+
+    @Overwrite
+    public void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(new Property[]{HANGING});
+        builder.add(WATERLOGGED);
+    }
+
+    public FluidState getFluidState(BlockState state) {
+        return (Boolean)state.get(WATERLOGGED) ? Fluids.WATER.getStill(false) : super.getFluidState(state);
+    }
+
+    static {
+        HANGING = Properties.HANGING;
+        WATERLOGGED = Properties.WATERLOGGED;
+        }
+}
