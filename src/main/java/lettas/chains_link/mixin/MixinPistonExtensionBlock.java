35031fb36983735c8a8010d03b95654fa145f55a
Andrew Knowles <ajknowles@optonline.net>
2020-07-11 16:30:51 -0400

    Delete MixinPistonExtensionBlock.java

--- src/main/java/lettas/chains_link/mixin/MixinPistonExtensionBlock.java
+++
@@ -1,9 +0,0 @@
-package lettas.chains_link.mixin;
-
-import net.minecraft.block.PistonExtensionBlock;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(PistonExtensionBlock.class)
-public class MixinPistonExtensionBlock {
-
-}
