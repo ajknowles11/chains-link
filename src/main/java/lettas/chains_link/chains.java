6595b613d2179eaf7004c043173d427a59d50d0c
Andrew Knowles <ajknowles@optonline.net>
2020-07-11 20:05:08 -0400

    Update chains.java

--- src/main/java/lettas/chains_link/chains.java
+++ src/main/java/lettas/chains_link/chains.java
@@ -6,7 +6,9 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class chains implements ModInitializer {
-
+	public static boolean isLanternGravity = false;
+	public static int pushLimit = 12;
+	
 	@Override
 	public void onInitialize() {}
 	
