bc8f4a5365e57fc1200240769bd0f6c7ce7e73aa
Andrew Knowles <ajknowles@optonline.net>
2020-08-17 00:21:25 -0400

    Removed redundant waterlogged property
    Minecraft 1.16.2 supports waterlogging lanterns, so mixins related to that property were unnecessary and removed

--- src/main/java/lettas/chains_link/mixin/clMixinLanternBlock.java
+++ src/main/java/lettas/chains_link/mixin/clMixinLanternBlock.java
@@ -1,63 +1,24 @@
 package lettas.chains_link.mixin;
 
 import net.minecraft.block.*;
-import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.piston.PistonBehavior;
-import net.minecraft.fluid.FluidState;
-import net.minecraft.fluid.Fluids;
-import net.minecraft.item.ItemPlacementContext;
-import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.Properties;
-import net.minecraft.state.property.Property;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
-import net.minecraft.world.WorldAccess;
 import net.minecraft.world.WorldView;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import lettas.chains_link.chains;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LanternBlock.class)
 public class clMixinLanternBlock extends Block implements Waterloggable {
     @Shadow public static final BooleanProperty HANGING;
-    private static final BooleanProperty WATERLOGGED;
 
     public clMixinLanternBlock(Settings settings) {
         super(settings);
     }
-
-    @Inject(method = "<init>*", at = @At("TAIL"))
-    public void clMixinLanternBlock(AbstractBlock.Settings settings, CallbackInfo ci) {
-        this.setDefaultState((BlockState)((BlockState)this.stateManager.getDefaultState()).with(WATERLOGGED, false).with(HANGING, false));
-    }
-
-    @Overwrite
-    public BlockState getPlacementState(ItemPlacementContext ctx) {
-        Direction[] var2 = ctx.getPlacementDirections();
-        int var3 = var2.length;
-        FluidState fluidState = ctx.getWorld().getFluidState(ctx.getBlockPos());
-        boolean bl = fluidState.getFluid() == Fluids.WATER;
-
-        for(int var4 = 0; var4 < var3; ++var4) {
-            Direction direction = var2[var4];
-            if (direction.getAxis() == Direction.Axis.Y) {
-                BlockState blockState = (BlockState)this.getDefaultState().with(WATERLOGGED, bl).with(HANGING, direction == Direction.UP);
-                if (blockState.canPlaceAt(ctx.getWorld(), ctx.getBlockPos())) {
-                    return blockState;
-                }
-            }
-        }
-
-
-
-        return null;
-    }
-
 
     @Overwrite
     public PistonBehavior getPistonBehavior(BlockState state) {
@@ -77,27 +38,7 @@ public class clMixinLanternBlock extends Block implements Waterloggable {
         return Block.sideCoversSmallSquare(world, pos.offset(direction), direction.getOpposite());
     }
 
-    @Overwrite
-    public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState newState, WorldAccess world, BlockPos pos, BlockPos posFrom) {
-        if ((Boolean)state.get(WATERLOGGED)) {
-            world.getFluidTickScheduler().schedule(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
-        }
-        return attachedDirection(state).getOpposite() == direction && !state.canPlaceAt(world, pos) ? Blocks.AIR.getDefaultState() : super.getStateForNeighborUpdate(state, direction, newState, world, pos, posFrom);
-    }
-
-    @Overwrite
-    public void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        builder.add(new Property[]{HANGING});
-        builder.add(new Property[]{WATERLOGGED});
-    }
-
-    @Override
-    public FluidState getFluidState(BlockState state) {
-        return (Boolean)state.get(WATERLOGGED) ? Fluids.WATER.getStill(false) : super.getFluidState(state);
-    }
-
     static {
         HANGING = Properties.HANGING;
-        WATERLOGGED = Properties.WATERLOGGED;
         }
 }
