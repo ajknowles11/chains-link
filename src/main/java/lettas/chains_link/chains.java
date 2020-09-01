0aa54a1a5ac251c5c515b6f241307fae5f005d0d
Andrew Knowles <ajknowles@optonline.net>
2020-09-01 10:19:04 -0400

    Update chains.java
    Added direction properties to be used by chains

--- src/main/java/lettas/chains_link/chains.java
+++ src/main/java/lettas/chains_link/chains.java
@@ -1,13 +1,19 @@
 package lettas.chains_link;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.state.property.BooleanProperty;
 
 public class chains implements ModInitializer {
 	public static boolean isLanternGravity = false;
+	public static final BooleanProperty EDIT_DOWN = BooleanProperty.of("edit_down");
+	public static final BooleanProperty EDIT_UP = BooleanProperty.of("edit_up");
+	public static final BooleanProperty EDIT_NORTH = BooleanProperty.of("edit_north");
+	public static final BooleanProperty EDIT_SOUTH = BooleanProperty.of("edit_south");
+	public static final BooleanProperty EDIT_WEST = BooleanProperty.of("edit_west");
+	public static final BooleanProperty EDIT_EAST = BooleanProperty.of("edit_east");
 
 	@Override
 	public void onInitialize() {
 
 	}
-	
 }
